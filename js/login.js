21eaf93faa4872877c1b9ac9a29657874037561e
showrov4g <showrovg00@gmail.com>
2024-09-18 22:58:18 +0600

    giting phone number value from event handelar

--- js/login.js
+++ js/login.js
@@ -1,3 +1,10 @@
-document.getElementById("btn-login").addEventListener("click", function () {
-    
+// login button function add
+// step-1 setup event handelar
+document.getElementById("btn-login").addEventListener("click", function (event) {
+    // step-2 prevent default behavior (prevent reloading browser)
+    event.preventDefault();
+    console.log('login button clicked');
+    // step-3 get the phone number 
+    const phoneNumber = document.getElementById('phone-number').value;
+    console.log(phoneNumber);
 });
