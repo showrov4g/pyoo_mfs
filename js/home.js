83b87d7cd962963da5618345780b012b6078784d
showrov4g <showrovg00@gmail.com>
2024-09-19 00:57:04 +0600

    home.js file setup

---
+++ js/home.js
@@ -0,0 +1,12 @@
+// event listener for button click 
+// add money to the account
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    // prevent reload after from submit 
+    event.preventDefault();
+    // get money to the added account 
+    let tkAmount = document.getElementById('amount').value;
+    let pinNumber = document.getElementById('pin-number').value;
+    if(pinNumber === 1234){
+
+    }
+})
