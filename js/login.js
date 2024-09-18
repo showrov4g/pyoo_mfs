5e040437eefc52406d79b85651e3b80a34d2f781
showrov4g <showrovg00@gmail.com>
2024-09-18 23:54:04 +0600

    home page design setup

--- js/login.js
+++ js/login.js
@@ -3,8 +3,20 @@
 document.getElementById("btn-login").addEventListener("click", function (event) {
     // step-2 prevent default behavior (prevent reloading browser)
     event.preventDefault();
-    console.log('login button clicked');
     // step-3 get the phone number 
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
+
+    // this is not a perfect way . this is temporary
+    // step-4 phone number and pin validation check 
+    if(phoneNumber === "5" && pinNumber === "1234"){
+        console.log('you are login');
+        // step-5 allow user to use website
+        window.location.href = '/home.html';
+        
+    }else{
+        alert("wrong phone number or pin");
+    }
+
 });
