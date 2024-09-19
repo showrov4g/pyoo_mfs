ddd7d15249e1a93369db012c8e64a4ea2c0ea47b
showrov4g <showrovg00@gmail.com>
2024-09-19 08:43:26 +0600

    balance add function complete

--- js/home.js
+++ js/home.js
@@ -1,16 +1,27 @@
-// event listener for button click 
+// event listener for button click
 // add money to the account
-document.getElementById('btn-add-money').addEventListener('click', function(event){
-    // prevent reload after from submit 
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    // prevent reload after from submit
     event.preventDefault();
-    // get money to the added account 
-    let tkAmount = document.getElementById('amount').value;
-    let pinNumber = document.getElementById('pin-number').value;
+    // get money to the added account
+    let tkAmount = document.getElementById("amount").value;
+    let pinNumber = document.getElementById("pin-number").value;
 
-    // verify pin number 
-    if(pinNumber === '1234'){
-        console.log('adding money to your wallet')
-    }else{
-        alert('failed to add money to your wallet')
+    // verify pin number
+    if (pinNumber === "1234") {
+        const balance = document.getElementById('account-balance').innerText;
+        //  add tkAmount with balance
+        const tkAmountNumber = parseFloat(tkAmount);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = balanceNumber + tkAmountNumber;
+        console.log(newBalance);
+
+        // update the balance in the UI/ wallet
+        document.getElementById('account-balance').innerText = newBalance;
+         
+    } else {
+      alert("failed to add money to your wallet");
     }
-})
+  });
