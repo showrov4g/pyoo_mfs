8e2a95d58fa92c85da7503e8c0a272f3665189d9
showrov4g <showrovg00@gmail.com>
2024-09-19 09:33:56 +0600

    new js file add>

---
+++ js/addmoney.js
@@ -0,0 +1,10 @@
+/**
+ * 1. add event listener to the add money button (form submit);
+ * 2. make sure to preventDefault so that page dose't reload;
+ * 3. get the money user want to add and also get the pin number provided
+ * 4. verify the pin number. for wrong pin number ===> failed to add money;  for right pin allow to add money to your wallet.
+ * 5. get current balance
+ * 6. add money to be added with current balance.
+ * 7. Display/update the new balance in the UI/DOM
+ * 
+ */
